29290a47935e7b6ba9e463d535c3a440d5a814d1
Seu Nome <seu-email@exemplo.com>
2026-08-19 20:17:45 -0300

    Subindo nova versao

--- SurvivorGame/obj/Debug/net10.0/SurvivorGame.AssemblyInfo.cs
+++ SurvivorGame/obj/Debug/net10.0/SurvivorGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurvivorGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbcaac6c35aa1bd45991bfb7ab695036f275be4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7fc6d588bf21b89e700aa660744b2080821f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurvivorGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurvivorGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
