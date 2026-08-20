0afaa2b5339d21b3531f2dfe771f48bc64c59302
ddanjos <ddanjos.dev@gmail.com>
2026-08-19 21:18:35 -0300

    Adicao de sprints icones

--- SurvivorGame/obj/Debug/net10.0/SurvivorGame.AssemblyInfo.cs
+++ SurvivorGame/obj/Debug/net10.0/SurvivorGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurvivorGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20605f0d1e8f26a6a4d805c0ed45230690786d10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76a4e3da52e366a2f2f728fc4205bc3cd4d2eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurvivorGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurvivorGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
